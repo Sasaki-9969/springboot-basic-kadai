b6b93eef86b48101001ad544c09272bb936e3850
Sasaki-9969 <minihito12@gmail.com>
2025-08-22 21:16:29 +0900

    kadai_formをコミット

---
+++ src/main/java/com/example/springkadaitodo/SpringKadaiForm1Application.java
@@ -0,0 +1,13 @@
+package com.example.springkadaitodo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringKadaiForm1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringKadaiForm1Application.class, args);
+	}
+
+}
